4b5f26c18231b64bb6cde4b0f068a7c2dd08463f
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-10 16:52:01 +0200

    Completion of the for loop, while and pushing objects into the array

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ const initialTasks = [
     status: "done",
   },
 ];
+
+for (let i = 0; i < 3; i++) {
+  const id = initialTasks.length + 1;
+  const newTitle = prompt(`Enter the title of task ${id}`);
+  const newDesc = prompt(`Enter the description of task ${id}`);
+  let newProg = prompt(`Enter the progress of task ${id}`).toLowerCase();
+
+  while (newProg !== "todo" && newProg !== "doing" && newProg !== "done") {
+    alert("Invalid status. Please enter 'todo' 'doing', or 'done'.");
+    newProg = prompt(`Enter the progression for task ${id}`).toLowerCase();
+  }
+  const task = {
+    id: id,
+    title: newTitle,
+    description: newDesc,
+    status: newProg,
+  };
+  initialTasks.push(task);
+}
+
+alert("There are enough tasks on your board, please check them in the console");
+
+console.log("All Tasks:", initialTasks);
+
+const completedTasks = initialTasks.filter((task) => task.status === "done");
+console.log("Completed Tasks", completedTasks);
