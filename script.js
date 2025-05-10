f609e7e0ccfa542ec43971ea52aac6256cfc6c6a
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-10 20:42:15 +0200

    Added comments/updated variable names for clarity

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const initialTasks = [
+//Creating an array for the initial tasks
+const taskList = [
   {
     id: 1,
     title: "Launch Epic Career",
@@ -20,28 +21,36 @@ const initialTasks = [
   },
 ];
 
+//the for loop is meant to run 3 times to represent the amount of tasks needed
 for (let i = 0; i < 3; i++) {
-  const id = initialTasks.length + 1;
+  const id = taskList.length + 1; //This creates the unique ID for each task as the number increases by 1 through each loop
   const newTitle = prompt(`Enter the title of task ${id}`);
   const newDesc = prompt(`Enter the description of task ${id}`);
-  let newProg = prompt(`Enter the progress of task ${id}`).toLowerCase();
+  let newStatus = prompt(`Enter the progress of task ${id}`).toLowerCase(); //The toLowerCase turns any input into lowercase be it upper, lower or a mix of the two
 
-  while (newProg !== "todo" && newProg !== "doing" && newProg !== "done") {
+  while (
+    newStatus !== "todo" &&
+    newStatus !== "doing" &&
+    newStatus !== "done"
+  ) {
+    //This is to make sure that the input is valid
     alert("Invalid status. Please enter 'todo' 'doing', or 'done'.");
-    newProg = prompt(`Enter the progression for task ${id}`).toLowerCase();
+    newStatus = prompt(`Enter the progression for task ${id}`).toLowerCase();
   }
+
+  //Making the task object and pushing it into the array
   const task = {
     id: id,
     title: newTitle,
     description: newDesc,
-    status: newProg,
+    status: newStatus,
   };
-  initialTasks.push(task);
+  taskList.push(task); //Adding a new task to the taskList
 }
-
+//This is to tell the user that they've reached the max number of new entries
 alert("There are enough tasks on your board, please check them in the console");
 
-console.log("All Tasks:", initialTasks);
+console.log("All Tasks:", taskList);
 
-const completedTasks = initialTasks.filter((task) => task.status === "done");
+const completedTasks = taskList.filter((task) => task.status === "done");
 console.log("Completed Tasks", completedTasks);
